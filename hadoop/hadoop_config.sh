84f6ee2bcda3499f551f7481e400d9469047a109
Fredy Álvarez <fre.alvarezb@gmail.com>
2019-08-22 23:20:07 -0500

    Hadoop config

---
+++ hadoop/hadoop_config.sh
@@ -0,0 +1,16 @@
+
+# 4. Set env vars
+cd 
+echo "PATH=/home/hadoop/hadoop/bin:/home/hadoop/hadoop/sbin:\$PATH" >> .profile
+echo "export HADOOP_HOME=\$HOME/hadoop" >> .bashrc
+echo "export PATH=\$PATH:\$HADOOP_HOME/bin:\$HADOOP_HOME/sbin" >> .bashrc
+
+# 5. Set java home
+JAVA_HOME=# Add here your java dir e.g.: /usr/lib/jvm/java-8-openjdk/jre
+echo "export JAVA_HOME=$JAVA_HOME" >> .bashrc
+
+# 6. Copy config files
+HADOOP_CONFIG_DIR=$HOME/hadoop/etc/hadoop/
+cp hadoop_conf/* $HADOOP_CONFIG_DIR
+
+
---
+++ hadoop/install_hadoop.sh
@@ -0,0 +1,5 @@
+# 3. Download and install hadoop
+cd
+wget http://apache.cs.utah.edu/hadoop/common/current/hadoop-3.1.2.tar.gz
+tar -xzf hadoop-3.1.2.tar.gz
+mv hadoop-3.1.2 hadoop
